938f5a14f8a9d3badd83d3d149d44dc0149fbb9a
Michael F. <51346737+Megafont@users.noreply.github.com>
2026-06-16 05:28:57 -0500

    Changed logging level of EntitiyFrameworkCore.Database.Command and refactored some more code from Program.cs into DataExtensions.cs.

--- GameStore.API/Data/DataExtensions.cs
+++ GameStore.API/Data/DataExtensions.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 
 using GameStore.API.Data;
+using GameStore.API.Models;
 
 namespace GameStore.API;
 
@@ -13,5 +14,39 @@ public static class DataExtensions
         using var scope = app.Services.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
         dbContext.Database.Migrate(); // If this line causes an error that Migrate() is not found, make sure you have the Microsoft.EntityFrameworkCore Nuget package installed and that you have the "using Microsoft.EntityFrameworkCore;" statement at the top of this file.
+    }
+
+    public static void AddGameStoreDb(this WebApplicationBuilder builder)
+    {
+        // Load the connection string from appSettings.json.
+        // NOTE: Connection strings in the appsettings.json file can be overriden. For example, if you set an environment variable named "ConnectionStrings__GameStore" to a different value, that value will be used instead of the one in appsettings.json. This is a common way to provide different connection strings for development and production environments without having to change the code or the appsettings.json file.
+        //       You can do this by going into the terminal, and typing "$env:ConnectionStrings__GameStore="Data Source=Production.db"
+        //       If you then run the program again, it will create a database named Production.db instead of the normal GameStore.db specified in appsettings.json. This is a very useful feature for managing different environments like development, staging, and production.
+        //       This environment variable will persist until you kill the terminal tab you created it in. If you then run the program again, it will go back to using the connection string from appsettings.json and create a database named GameStore.db if it doesn't already exist in the project.
+        //       In VS Code, you can kill the termain by going to the terminal tab, and in its right pane, right-click the terminal you want to kill and select "Kill Terminal". You can also just select the terminal in the list and press Del.
+        var connectionString = builder.Configuration.GetConnectionString("GameStore");
+
+        builder.Services.AddSqlite<GameStoreContext>(
+            connectionString,
+            optionsAction: options => options.UseSeeding((context, _) =>
+            {
+                // If the Genres table is empty, seed it with some initial genres.
+                if (!context.Set<Genre>().Any())
+                {
+                    context.Set<Genre>().AddRange(
+                        new Genre { Name = "Fighting" },
+                        new Genre { Name = "RPG" },
+                        new Genre { Name = "Platformer" },
+                        new Genre { Name = "Racing" },
+                        new Genre { Name = "Sports" },
+                        new Genre { Name = "Action-Adventure" },
+                        new Genre { Name = "Metroidvania" }
+                    );
+
+                    context.SaveChanges();
+                }
+            })
+        );
+
     }
 }
--- GameStore.API/Program.cs
+++ GameStore.API/Program.cs
@@ -3,14 +3,11 @@
 // https://www.youtube.com/watch?v=YbRe4iIVYJk
 
 using GameStore.API;
-using GameStore.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation(); // This adds validation services to the DI container, which allows us to use data annotations for validating our DTOs. These are the attributes like [Required] that appear in the DTO classes.
-
-var connectionString = "Data Source=GameStore.db";
-builder.Services.AddSqlite<GameStoreContext>(connectionString);
+builder.AddGameStoreDb(); // This is the second extension method we declared in DataExtensions.cs that configures the GameStoreContext to use SQLite and also seeds the Genres table with some initial data if it's empty.
 
 
 var app = builder.Build();
@@ -20,4 +17,5 @@ app.MapGamesEndpoints(); // This is the extension method we declared in GamesEnd
 app.MigrateDb(); // This is the extension method we declared in DataExtensions.cs that applies any pending migrations to the database. This ensures that our database schema is up to date with our EF Core models in the Data folder when the application starts.
 
 app.Run();
+
 
